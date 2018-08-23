4896519010b28bed980a8dcb1a6d43e657de0005
Rey David <rdominguez@tecnologer.net>
2018-08-22 23:12:30 -0700

    Separate main code of library

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"regexp"
 	"strings"
+
+	"github.com/tecnologer/romantoarabic/num"
 )
 
 var err error
@@ -26,13 +27,13 @@ func main() {
 		fmt.Print("Escribe el numero romano correcto: ")
 		for err != nil || romanInput == "" || !isCorrect {
 			_, err = fmt.Scan(&romanInput)
-			isCorrect = IsRoman(romanInput)
+			isCorrect = num.IsRoman(romanInput)
 			if !isCorrect {
 				fmt.Printf("\"%s\" no es un numero romano, intenta de nuevo: ", romanInput)
 			}
 		}
 
-		fmt.Printf("%s es igual a %d\n", strings.ToUpper(romanInput), ParseRoman(romanInput))
+		fmt.Printf("%s es igual a %d\n", strings.ToUpper(romanInput), num.ParseRoman(romanInput))
 	case 2:
 		var arabicInput int
 		fmt.Print("Escribe el numero arabigo correcto (mayor a 0 y menor a 4000): ")
@@ -40,88 +41,8 @@ func main() {
 			_, err = fmt.Scan(&arabicInput)
 		}
 
-		fmt.Printf("%d es igual a %s\n", arabicInput, ParseArabic(arabicInput))
+		fmt.Printf("%d es igual a %s\n", arabicInput, num.ParseArabic(arabicInput))
 	}
 	// var arabicInput = 3999
 	// fmt.Printf("%d es igual a %s", arabicInput, ParseArabic(arabicInput))
-}
-
-//IsRoman check if a string is a roman number
-func IsRoman(roman string) bool {
-	var regex = "(?i)^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"
-	match, _ := regexp.MatchString(regex, roman)
-	return match
-}
-
-//ParseRoman Parse arabic number to roman number
-func ParseRoman(roman string) int {
-	roman = strings.ToUpper(roman)
-	var romanValues = map[string]int{
-		"I": 1,
-		"V": 5,
-		"X": 10,
-		"L": 50,
-		"C": 100,
-		"D": 500,
-		"M": 1000,
-	}
-
-	var finalNumber int
-	for i := 0; i < len(roman); i++ {
-		letter := string(roman[i])
-
-		var nextLetter string
-		if (i + 1) < len(roman) {
-			nextLetter = string(roman[i+1])
-		}
-
-		if nextLetter != "" {
-			if romanValues[letter] >= romanValues[nextLetter] {
-				finalNumber += romanValues[letter]
-			} else {
-				finalNumber -= romanValues[letter]
-			}
-		} else {
-			finalNumber += romanValues[letter]
-		}
-	}
-
-	return finalNumber
-}
-
-//ParseArabic Parse Roman number to Arabic number
-func ParseArabic(arabic int) string {
-	var roman string
-
-	var romanValues = map[string]int{
-		"M":  1000,
-		"CM": 900,
-		"D":  500,
-		"CD": 400,
-		"C":  100,
-		"XC": 90,
-		"L":  50,
-		"XL": 40,
-		"X":  10,
-		"IX": 9,
-		"V":  5,
-		"IV": 4,
-		"I":  1,
-	}
-
-	var order = []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
-
-	for _, key := range order {
-		var value = romanValues[key]
-		var qty = arabic / value
-		if qty > 3 {
-			qty = 3
-		}
-
-		for i := 0; i < qty; i++ {
-			roman += key
-		}
-		arabic = arabic - (qty * value)
-	}
-	return roman
 }
